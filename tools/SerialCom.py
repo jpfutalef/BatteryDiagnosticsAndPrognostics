58aa9c8c4b7f0d63eb3e683bd706de9fe3704954
Juan Pablo Futalef <jpfutalef@gmail.com>
2019-11-11 15:55:14 -0300

    Improvement in serial communication

--- tools/SerialCom.py
+++ tools/SerialCom.py
@@ -2,102 +2,151 @@
 
 import serial
 import time
-import sys, signal
+import sys
+import signal
 import numpy as np
 import pandas as pd
 
+# User defined variables
 serial_port = 'COM1'
 
+
+# Useful funs
 def signal_handler(signal, frame):
     print('\nTerminando...')
-    sys.exit(0)
-
-class SerConfig():
-    def __init__(self):
-        self.baudrate = 9600
-        self.port = "COM1"
-        self.timeout = 1  # 1 segundo de timeout
 
 
-class Medicion():
-    def __init__(self, SerConfig, Ts=1):
+# Classes
+class SerialConfig:
+    def __init__(self, baud_rate=9600, port="COM1", time_out=1):
+        """
+        A class to store serial communication info.
+        :param baud_rate:   communication baud rate. By default 9600.
+        :param port:    the port where the serial device is connected to. By default 'COM1'
+        :param time_out:    communiaction timeout in seconds. By default 1 second.
+        """
+        self.baud_rate = baud_rate
+        self.port = port
+        self.timeout = time_out  # 1 segundo de timeout
+
+
+class LoadConfig:
+    def __init__(self, baud_rate=9600, port="COM1", time_out=1):
+        """
+        A class to store load info.
+        :param baud_rate:   communication baud rate. By default 9600.
+        :param port:    the port where the serial device is connected to. By default 'COM1'
+        :param time_out:    communiaction timeout in seconds. By default 1 second.
+        """
+        self.baudRate = baud_rate
+        self.port = port
+        self.timeout = time_out  # 1 segundo de timeout
+
+
+class ElectronicLoad:
+    def __init__(self, config_serial: SerialConfig, config_load, Ts=1, measurement_folder_path="../data/",
+                 measurement_file_name="fileName"):
+        # Measurement variables
         self.Ts = Ts
         self.V = []
         self.I = []
         self.t = []
 
-        self.Ser = serial.Serial()
-        self.Ser.baudrate = SerConfig.baudrate
-        self.Ser.port = SerConfig.port
-        self.Ser.timeout = SerConfig.timeout
+        # The path where measurements will be stored
+        self.measurement_folder_path = measurement_folder_path
+        self.measurement_file_name = measurement_file_name
 
+        self.measurement_file_path = self.measurement_folder_path + self.measurement_file_name + ".csv"
+
+        # Create file where measured values will be stored. The file contains a header only.
+        pd.DataFrame(columns=('t', 'V', 'I')).to_csv(self.measurement_file_path)
+
+        # Serial communication handlers
+        self.serial = serial.Serial()
+        self.serial.baudrate = config_serial.baud_rate
+        self.serial.port = config_serial.port
+        self.serial.timeout = config_serial.timeout
+
+        # Initialize serial communication
+        self.begin()
+
+    def begin(self):
+        # Try opening the serial port
         try:
-            self.Ser.open()
+            self.serial.open()
+
         except Exception as e:
-            print("Error abriendo puerto serial: "+str(e))
-            exit()
+            print("Cannot open serial port: " + str(e))
+            sys.exit(1)
 
-        if self.Ser.isOpen():
-            self.Ser.flushInput()
-            self.Ser.flushOutput()
-            print("Puerto serial inicializado con exito")
+        # Clean buffer and check if everything is alright
+        if self.serial.is_open:
+            self.serial.reset_input_buffer()
+            self.serial.reset_output_buffer()
+            print("Serial port has been initialized successfully!\n",
+                  "Connected to port: " + str(self.serial.port))
         else:
-            print("No se pudo abrir el puerto serial")
-
-
-    def read_v(self):
-        # Leer voltaje desde el equipo
-        meas = self._request("MEAS:VOLT:ACDC?")
-        return meas
-
-    def read_i(self):
-        # Leer corriente desde el equipo
-        meas = self._request("MEAS:CURR:ACDC?")
-        return meas
+            print("Cannot open serial port.")
+            sys.exit(1)
 
     def _request(self, cmd):
-        self.Ser.write(cmd+"\r\n")
-        time.sleep(self.Ts/10)
+        toSend = bytes(cmd + "\n", 'utf-8')
+        self.serial.write(toSend)
+        time.sleep(self.Ts / 10)
         out = ''
-        while self.Ser.inWaiting() > 0:
-            out += self.Ser.read(1)
+        while self.serial.in_waiting > 0:
+            out += self.serial.read(1).decode("utf-8")  # TODO posiblemente mejor readline?
         if out != '':
             return out
         else:
             return "Timeout"
 
-    def set_load_config():
+    def read_voltage(self):
+        # Leer voltaje desde el equipo
+        meas = self._request("MEAS:VOLT?")
+        return meas
+
+    def read_current(self):
+        # Leer corriente desde el equipo
+        meas = self._request("MEAS:CURR?")
+        return meas
+
+    def set_load_config(self, config, method='loop'):
         # TODO Definir curva de carga y configurar a la maquina
         return 0
 
-    def begin(self):
-        # Realizar mediciones constantes cada Ts segundos
-        dummy = 1 # TODO buscar como parar el loop por medición obtenida
-        self.t_inicial = time.time()
-        t0 = self.t_inicial
-        idx = 0
-        while dummy:
+    def periodic_measurement(self, sample_time=1):
+        # Realizar mediciones constantes cada sample_time segundos
+
+        # Set signal handler
+        signal.signal(signal.SIGINT, signal_handler)
+
+        # Initialize first values and append
+        t0 = time.time()
+
+        # Iterate until user press ctrl+c
+        while True:
             t1 = time.time()
-            if t1-t0 >= self.Ts:
+            if t1 - t0 >= sample_time:
+                data = {'t': [t1],
+                        'V': [self.read_voltage()],
+                        'I': [self.read_current()]}
+                df = pd.DataFrame(data)
+                print(df)
+                df.to_csv(self.measurement_file_path, index=None, header=False, mode='a')
                 t0 = time.time()
-                self.V.append(self.read_v())
-                self.I.append(self.read_i())
-                self.t.append(t1 - self.t_inicial)
-                idx = idx + 1
-            signal.signal(signal.SIGINT, signal_handler)
 
     def pack_data(self):
         data = {'V': self.V,
                 'I': self.I,
                 't': self.t}
         df = pd.DataFrame(data)
-        df.to_csv(r'Measurement\export.csv', index=None, header=True)
+        print('Obtained data frame:\n', df)
+        df.to_csv(r'../data/labMeasurements/measurement.csv', index=None, header=True)
 
 
 if __name__ == "__main__":
-    conf = SerConfig()
-    Meas = Medicion(SerConfig)
-    Meas.set_load_config()
-    Meas.begin()
-    Meas.pack_data()
+    conf = SerialConfig(port="COM9")
+    N3300A = ElectronicLoad(conf, 10)
+    N3300A.periodic_measurement()
     sys.exit(0)
